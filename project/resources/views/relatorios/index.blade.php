c94dfd39b3eb1eb9b91bd2be3dc35ec77c7be22c
Alan Tavares <alan_tj@hotmail.com.br>
2022-04-04 15:44:45 -0300

    finalizacao relatorio

--- project/resources/views/relatorios/index.blade.php
+++ project/resources/views/relatorios/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Relatoritórios Financeiros</div>
+                <div class="card-header">Relatoritórios Financeiros das Mensalidades</div>
 
                 <div class="card-body">
                     @if (session('status'))
